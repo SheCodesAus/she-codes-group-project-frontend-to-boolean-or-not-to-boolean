0d8b493bfee2a9d43093eba137d40cf2f676d843
Mara Petra <62734302+mariapetra@users.noreply.github.com>
2022-05-25 21:29:37 +0800

    added basic hero code and styling with glow text

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+//styles
+import "./Hero.css"
+
+function Hero() {
+return(
+    <div className="hero">
+        <h1>Here we will have some <span class="hero-highlight"> kind </span> 
+        of quote with some <span class="hero-highlight">highlight</span>.
+        text!</h1>
+    </div>
+)
+}
+
+export default Hero;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,8 @@
 // Home page
 import React from "react";
 
+//components
+import Hero from "../components/Hero/Hero";
 import QRGenerator from "../components/QRCode/QRCode"
 
 //styles
@@ -8,10 +10,12 @@ import "../index.css"
 
 function HomePage() {
     return <>
+                <Hero />
+
         <h3>
         Under Construction..
         </h3>;
-        <QRGenerator />
+        {/* <QRGenerator /> */}
     </>
 }
 
