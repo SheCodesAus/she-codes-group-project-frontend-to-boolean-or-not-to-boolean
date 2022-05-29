31572fa616994472cd2cd3d07d4c932ea606f821
Mara Petra <62734302+mariapetra@users.noreply.github.com>
2022-05-29 21:40:44 +0800

    fix removed home page - re-added home page

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,38 @@
+// Home page
+import React, { useState } from "react";
+
+//components
+import Hero from "../components/Hero/HeroHome";
+import Modal from "../components/GlobalModal/GlobalModal";
+import Footer from "../components/Footer/Footer";
+
+//styles
+import "../index.css"
+import "./HomePage.css"
+
+export default function HomePage() {
+    //function for shoing modal
+    const [show, setShow] = useState(false);
+
+    return <>
+        {/* start hero section */}
+        <div>
+            <Hero />
+        </div>
+        {/* end hero section */}
+
+        {/* start modal button*/}
+        <div className="ModalApp">
+            <button onClick={() => setShow(true) }>Show Modal</button>
+            {/* we pass the title as a prop and the content as a child because 
+            we will have multiple elements inside the content in the future. */}
+            <Modal title="My Modal"onClose={() => setShow(false)} show={show} />
+            <p>This is modal body</p>
+            <Modal />
+        </div>
+        {/* end modal button */}
+
+        <Footer />
+    </>
+
+}
