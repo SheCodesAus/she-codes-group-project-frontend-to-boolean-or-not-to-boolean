6866743725427754ddb3681b27b78c9c2c560056
snail-Py <cs.legoff@gmail.com>
2022-05-31 20:17:01 +1000

    create win wall page styling

--- src/pages/CreateWinWallPage.jsx
+++ src/pages/CreateWinWallPage.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 import WinWallForm from "../components/WinWallForm/WinWallForm"
+import "../pages/WinWallPage.css"
+
 
 
 
 function CreateWinWallPage() {
     return (
     
-    <div>
+        <div className="main-section">
             <h1>Create a new Win Wall</h1>
+            <p className="main-section--description">
+                Create a new win wall page to organise your sticky notes. </p>
+
             <WinWallForm />
         </div>
         
