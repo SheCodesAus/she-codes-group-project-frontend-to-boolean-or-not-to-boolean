82e78ae1d624106c53a973bd8aea5ffa4d3c0baf
Kristie <kmlbrandis@gmail.com>
2022-06-03 14:45:44 +0800

    tool tips!

--- src/components/SheCoderCard/SheCoderCard.jsx
+++ src/components/SheCoderCard/SheCoderCard.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import Tooltip from "../ToolTips/ToolTip";
+
 // Imports
 import SuperUser from "../images/icons/super-logo.png";
 import AdminUser from "../images/icons/system-admin.png";
@@ -16,16 +18,24 @@ function SheCoderCard( {userData} ) {
                 <img src={userData.avatar} alt="user avatar"/>
                 <h3>{userData.first_name}</h3>
             </Link>
+                 <Tooltip content="   Superuser  " direction="right">
                 <h4>{userData.is_superuser && 
                     <img src={SuperUser} alt="super-user"/>}
                 </h4>
+                </Tooltip>
+                <Tooltip content="  Admin " direction="right">
                 <h4>{userData.is_shecodes_admin &&                 
                     <img src={AdminUser} alt="admin-user"/>}
                 </h4>
+                </Tooltip>
+                <Tooltip content="  Approver " direction="right">
                 <h4>{userData.is_approver && 
                     <img src={Approver} alt="approver-user"/>}
                 </h4>
+                </Tooltip>
+                <Tooltip content="  You are Amazing! " direction="right">
                 <h4><img src={WonderWoman} alt="user"/></h4>
+                </Tooltip>
          </div>
     );
 }
