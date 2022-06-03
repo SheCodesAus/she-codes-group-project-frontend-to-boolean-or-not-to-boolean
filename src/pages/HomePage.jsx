a97ef897694a3d4bd34c27d4427c5306cda150ec
Mara Petra <62734302+mariapetra@users.noreply.github.com>
2022-06-03 18:26:00 +0800

    home page is completed

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -21,25 +21,29 @@ export default function HomePage() {
         <div className="home-text">
             <p>
                 <span className="highlight-text">She Codes WinWall </span><br></br>
-                     is a space for you to share your experiences with us and the rest of the <span clasName="alt-color-text">She Codes community</span>.
+                     is a space for you to share your experiences with us and the rest of the <span className="alt-color-text">She Codes community</span>.
                      Collaboration is at the heart of everything we do at She Codes and we welcome you to join us as we
-                     <span clasName="alt-color-text"> learn and grow together</span> in our mission to to 
-                     <span clasName="alt-color-text"> inspire, teach, and build a community</span> around tech for women in 
+                     <span className="alt-color-text"> learn and grow together</span> in our mission to to 
+                     <span className="alt-color-text"> inspire, teach, and build a community</span> around tech for women in 
                       Australia. Leading to ongoing career pathways to make a tangible difference to diversity in the tech 
                       industry. Our goal is to inspire 100,000 women across Australia by 2025... and we hope you will be one of them. 
-                      <span clasName="alt-color-text"> Thank you </span>for being a part of our community.
+                      <span className="alt-color-text"> Thank you </span>for being a part of our community.
             </p>
         </div>
 
         {/* start modal buttons*/}
         <section className="modal-section">
-            <h2>Looking for your event? Click on the subject you studied to be taken to you sticky note</h2>
-            < DjangoWall />
-            < FlashWall />
-            < PythonWall />
-            < HTMLWall />
-            < JavascriptWall />
-            < ReactWall />
+            <container clasName="modal-container">
+                < DjangoWall />
+                < FlashWall />
+                < PythonWall />
+                < HTMLWall />
+                < JavascriptWall />
+                < ReactWall />
+            </container>
+            <p className="home-modal-body"> <span className="highlight-text">Ready to share your experience?</span>
+            Click on the event you attended 
+                to be taken to the sticky note page</p> 
         </section>
 
 
