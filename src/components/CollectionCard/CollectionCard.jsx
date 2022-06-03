4e06d5778f9971752de777dca778b932f1290178
HannahHollywood <70626629+HannahHollywood@users.noreply.github.com>
2022-06-03 13:07:00 +0800

    Auth permissions completed - WinWalls & Collections!

--- src/components/CollectionCard/CollectionCard.jsx
+++ src/components/CollectionCard/CollectionCard.jsx
@@ -1,20 +1,25 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import EditCollectionForm from "../EditCollectionForm/EditCollectionForm";
 import "./CollectionCard.css";
 
 function CollectionCard(props) {
   
     const { collectionData } = props;
+
+    const SuperUser = window.localStorage.getItem("is_superuser");
+    const Admin = window.localStorage.getItem("is_shecodes_admin");
+   
+    const IsAdmin = (Admin == 'true');
+    const IsSuperUser = (SuperUser == 'true');
   
     return (
     <div className="card">
         <Link to={`/collection/${collectionData.id}/`}>
         <h3 className="card--title">{collectionData.title}</h3>
-        <img src={collectionData.image} />
+        <img src={collectionData.image} alt="collection-img"/>
         </Link>
-        <button><Link to={`/collection/${collectionData.id}/`}>View collection</Link></button>
-        <button><Link to={`/collection/${collectionData.id}/edit`}>Edit collection</Link></button>
+        <button><Link to={`/collection/${collectionData.id}/`}>View Collection</Link></button>
+        {(IsAdmin || IsSuperUser) &&<button><Link to={`/collection/${collectionData.id}/edit`}>Edit Collection</Link></button>}
 
     </div>
   );
--- src/components/WinWallCard/WinWallCard.jsx
+++ src/components/WinWallCard/WinWallCard.jsx
@@ -6,7 +6,15 @@ import "../../components/CollectionCard/CollectionCard.css"
 function WinWallCard(props) {
   
     const { winwallData } = props;
-  
+
+    const SuperUser = window.localStorage.getItem("is_superuser");
+    const Admin = window.localStorage.getItem("is_shecodes_admin");
+    const Approver = window.localStorage.getItem("is_approver");
+   
+    const IsAdmin = (Admin == 'true');
+    const IsApprover = (Approver == 'true');
+    const IsSuperUser = (SuperUser == 'true');
+
     return (
 
     <div className="winwall-card">
@@ -14,8 +22,8 @@ function WinWallCard(props) {
         <img src={winwallData.image} alt="winwallimage" />
         <h3>{winwallData.title}</h3>
         </Link>
-        <button><Link to={`/win-wall/${winwallData.id}/`}>View win wall</Link></button>
-        <button><Link to={`/win-wall/${winwallData.id}/edit`}>Edit win wall</Link></button>
+        <button><Link to={`/win-wall/${winwallData.id}/`}>View Win Wall</Link></button>
+        {(IsAdmin || IsSuperUser || IsApprover) &&<button><Link to={`/win-wall/${winwallData.id}/edit`}>Edit Win Wall</Link></button>}
 
     </div>
   );
